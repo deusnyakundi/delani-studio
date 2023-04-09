fe2fb4b9ee17cc5aee550e8766c7c75571aee946
Deus Nyakundi <deusnyakundi@Alchemist-2.local>
2023-04-09 09:45:08 +0300

    Adding goggle fonts and initializing the setup

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+$(document).ready(()=>{
+    $("#one").mouseleave(()=> { 
+        console.log("I hovered above one");
+    });
+    $("#two").mouseenter(()=>{
+        console.log("I have entered the room")
+    }
+    
+    )
+
+
+})
